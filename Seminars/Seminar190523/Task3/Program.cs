47d3ae41db8d82efa6eb9dd23451e92a9d352384
AndreyAlVS <aavasquezs@gmail.com>
2023-05-21 00:16:38 +0300

    problem 3 solved

--- Seminars/Seminar190523/Task3/Program.cs
+++ Seminars/Seminar190523/Task3/Program.cs
@@ -4,3 +4,15 @@
 // -3 -> нет
 // 7 -> нет
 
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
