12f933323201b5af50a1e231f081952354ee8b84
AndreyAlVS <aavasquezs@gmail.com>
2023-05-21 21:44:08 +0300

    task 4

---
+++ Seminars/Seminar190523/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= num1 )
+{
+System.Console.Write(count + " ");
+count = count + 2;
+}
+if (num1 <= 1)
+{
+   System.Console.Write("Введите поожительное больше единицы"); 
+}
