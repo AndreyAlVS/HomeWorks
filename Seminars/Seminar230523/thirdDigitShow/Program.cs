cf0e81192dd5db0abd438603cab09af888f21fe5
AndreyAlVS <aavasquezs@gmail.com>
2023-05-25 11:34:52 +0300

    third Digit Show solved

---
+++ Seminars/Seminar230523/thirdDigitShow/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, кторая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+while (num1 > 999)
+{
+    num1 = num1 / 10;
+}
+
+if (num1 <= 99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    num1 = num1 % 10;
+    Console.WriteLine(num1);
+}
