8da97c39038e5dfd287e20042e61ded1bbe82738
AndreyAlVS <aavasquezs@gmail.com>
2023-05-25 13:09:20 +0300

    new

--- Seminars/Seminar230523/secondDigitRem/Program.cs
+++ Seminars/Seminar230523/secondDigitRem/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача необязательная 1: 
+// на входе целое или вещественное число, 
+// надо удалить вторую цифру слева целой части этого числа.
+
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+int num = new Random().Next();
+double r = Convert.ToDouble(x.Next(-10000, 10000)/10000.0);
+System.Console.WriteLine(r);
