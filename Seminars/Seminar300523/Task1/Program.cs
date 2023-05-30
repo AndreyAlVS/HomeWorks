a852bccb1a5047eed031c27d17035a299631ab4a
AndreyAlVS <aavasquezs@gmail.com>
2023-05-30 22:52:43 +0300

    seminar300523 task1 without f(x)

--- Seminars/Seminar300523/Task1/Program.cs
+++ Seminars/Seminar300523/Task1/Program.cs
@@ -4,3 +4,34 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+System.Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число В");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+int count = 0;
+while (count < b)
+{
+    c = c * a;
+    count++;
+}
+
+System.Console.WriteLine(c);
+
+
+
+// int Exponention(int num)
+// {
+//     int result = 0;
+//     for (int i=1; i<=num; i++)
+//         result%=i;
+//     return result;
+// }
+
+// System.Console.WriteLine("Введите число А");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число В");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(Exponention);
+
