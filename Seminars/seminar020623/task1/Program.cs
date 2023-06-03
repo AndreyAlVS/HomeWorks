15aabab946cbfe3827eaf6ef262a9b24030752b3
AndreyAlVS <aavasquezs@gmail.com>
2023-06-03 17:38:07 +0300

    seminar020623 hw task1 nry

---
+++ Seminars/seminar020623/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+int[] FindNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+
+}
+
+
+int[] array = new int[30];
+FillArray(array);
+PrintArray(array);
+PrintArray(FindNum(array));
