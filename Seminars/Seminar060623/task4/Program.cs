1007ea8ff388e9b80a1e7731a6051b31aa3d9f19
AndreyAlVS <aavasquezs@gmail.com>
2023-06-08 09:53:47 +0300

    debug all HW

--- Seminars/Seminar060623/task4/Program.cs
+++ Seminars/Seminar060623/task4/Program.cs
@@ -11,3 +11,39 @@
 // [1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1 ] => [1, 5] так как здесь есть числа от 1 до 5 и эта последовательность длиннее чем от 7 до 8
 
 // [1, 5, 3, 4, 1, 7, 8 , 15 , 1 ] => [3, 5] так как здесь есть числа от 3 до 5 и эта последовательность длиннее чем от 7 до 8
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+
+        array[i] = new Random().Next(-10, 100);
+
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+
+        Console.Write($"{item}, ");
+    System.Console.WriteLine();
+
+}
+
+int SumOddNumber(int[] array)
+{
+    int sumOdd = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        sumOdd = sumOdd + array[i];
+    }
+    return sumOdd;
+}
+
+
+System.Console.WriteLine("Введите размерность массива ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+FillArray(array);
+PrintArray(array);
+SumOddNumber(array);
+System.Console.WriteLine(SumOddNumber(array));
--- Seminars/Seminar230523/shau/Program.cs
+++ Seminars/Seminar230523/shau/Program.cs
@@ -1,8 +1,8 @@
-﻿int num = new Random().Next(1, 4);
+﻿// int num = new Random().Next(1, 4);
 
-if (num == 1)
-    System.Console.WriteLine("Дошик");
-else if (num == 2)
-    System.Console.WriteLine("Столовка");
-else
-    System.Console.WriteLine("Шавуха");
+// if (num == 1)
+//     System.Console.WriteLine("Дошик");
+// else if (num == 2)
+//     System.Console.WriteLine("Столовка");
+// else
+//     System.Console.WriteLine("Шавуха");
