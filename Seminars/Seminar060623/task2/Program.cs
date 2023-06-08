796d23b97f56fe48110079e20a8e44dc1d5e8dc1
AndreyAlVS <aavasquezs@gmail.com>
2023-06-08 15:30:14 +0300

    s0606230 t2_2

--- Seminars/Seminar060623/task2/Program.cs
+++ Seminars/Seminar060623/task2/Program.cs
@@ -2,24 +2,6 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-
-
-
-// System.Console.WriteLine("Введите число b1");
-// int b1 = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Введите число k1");
-// int k1 = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Введите число b2");
-// int b2 = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Введите число k2");
-// int k2 = Convert.ToInt32(Console.ReadLine());
-
-
-// double x = (4-2)/(5-9);
-// double y = (5*(4-2))/(5-9)+2;
-
 
 double findX(double[] array)
 {
