c56bee2a66e571a0608e90828a19b4db4750e5c8
AndreyAlVS <aavasquezs@gmail.com>
2023-06-15 22:35:22 +0300

    s090623 t3s

--- Seminar090623/task2/Program.cs
+++ Seminar090623/task2/Program.cs
@@ -9,3 +9,49 @@
 // 8 4 2 4
 
 // 17 -> такого числа в массиве нет
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+int IndexOf(int[,] array, int num)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == array[i, j])
+            sum = sum + array[i, j];
+            sum = sum/array.GetLength(0);
+
+        }
+return sum;
+}
+
+
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов ");
+int cols = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите искомое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+IndexOf(array, num);
+
--- Seminar090623/task3/Program.cs
+++ Seminar090623/task3/Program.cs
@@ -5,3 +5,48 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+void Average(int[,] array)
+{
+    System.Console.WriteLine("Среднее арифметическое каждого столбца:");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int count = 0;
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+            count++;
+        }
+        Console.Write($"{sum / (count):F1};    ");
+    }
+}
+
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Average(array);
